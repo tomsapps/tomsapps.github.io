cbde419740aa50c8774ce19e96b8fe11cb2f79dd
tomsapps <tomsok97@gmail.com>
2016-09-20 16:49:45 -0500

    Fix mobile layout

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,25 +3,23 @@ import { Link } from 'react-router';
 
 const Nav = () => {
   return (
-    <nav className="navbar pull-xs-right " >
-      <ul className="nav nav-link-pad">
+    <nav>
         {document.location.pathname === '/' ? '' : homeLink() }
-        <li className="nav-link">
+        <div>
           <a href="https://registry.jsonresume.org/tomsapps" target="_blank">Resume</a>
-        </li>
-        <li className="nav-link">
+        </div>
+        <div>
           <a href="https://github.com/tomsapps" target="_blank">GitHub</a>
-        </li>
-      </ul>
+        </div>
     </nav>
   );
 };
 
 function homeLink() {
   return (
-    <li className="nav-link">
+    <div>
       <Link to="/">Home</Link>
-    </li>
+    </div>
   );
 }
 
